01bcc9f8b0298f79af60b7fdf84b57f769abadea
Tagir Fayrushin <fayrushintagir@gmail.com>
2025-04-20 22:07:39 +0300

    Initial commit

---
+++ SmartBudget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartBudget
+//
+//  Created by Тагир Файрушин on 20.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
