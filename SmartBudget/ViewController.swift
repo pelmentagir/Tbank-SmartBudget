7d181ed4374426285ccb407a23f09a9e3e396ced
Tagir Fayrushin <fayrushintagir@gmail.com>
2025-04-20 22:47:31 +0300

    add swiftlint and snapkit

--- SmartBudget/ViewController.swift
+++ SmartBudget/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  SmartBudget
-//
-//  Created by Тагир Файрушин on 20.04.2025.
-//
-
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
